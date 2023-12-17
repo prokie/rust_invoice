46efec212ea054bb871d83a82ccd90ce9540a297
Pontus Thomsson <pontusthomsson@gmail.com>
2023-12-17 16:19:26 +0100

    Some progress on leptos tutorial

--- README.md
+++
@@ -1,7 +0,0 @@
-# rust_invoice
-
-Invoice website with backend written in Rust.
-
-
-    docker build -t test .
-    docker run -p 8080:8080 test
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
-mod app;
-
-use app::App;
+use leptos::*;
 
 fn main() {
-    yew::start_app::<App>();
+    leptos::mount_to_body(|| view! { <App/> })
 }
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
+    let values = vec![0, 1, 2];
+    view! {
+        <button
+            on:click=move |_| { set_count.update(|n| *n += 1) }
+            class:red=move || count() % 2 == 1
+        >
+            "Click me: "
+            {count}
+        </button>
+
+        <ProgressBar progress=count/>
+
+        <ProgressBar progress=Signal::derive(double_count)/>
+
+        <ul>{values.into_iter().map(|n| view! { <li>{n}</li> }).collect_view()}</ul>
+    }
+}
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar(
+    /// How much progress should be displayed.
+    #[prop(into)]
+    progress: Signal<i32>,
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+) -> impl IntoView {
+    view! { <progress max=max value=progress></progress> }
+}
+
